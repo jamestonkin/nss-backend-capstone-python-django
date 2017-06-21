25958be0c8d23b439ed34575a7beb46c3f8d561e
James Tonkin <jameswtonkin@gmail.com>
2017-06-21 13:18:33 -0500

    completed voting pages and added styling

---
+++ nashlegislature/website/views/billvoting_view.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from website.models import BillLegislationSynopsis, BillVoting
+
+
+def bill_voting(request, billlegislationsynopsis_id):
+    """
+    purpose: gets the bill voting data and displays on billvoting.html.
+    author: James Tonkin
+    args: request allows Django to see session data
+    returns: Combines billvoting.html with voting dictionaries and returns
+    the request with that rendered text.
+    """
+    template_name = 'billvoting.html'
+    current_bill = BillLegislationSynopsis.objects.get(pk=billlegislationsynopsis_id)
+
+    yes_votes = BillVoting.objects.all().filter(
+        bill_legislation_synopsis_id=billlegislationsynopsis_id, 
+        vote=1
+    )
+
+    no_votes = BillVoting.objects.all().filter(
+        bill_legislation_synopsis_id=billlegislationsynopsis_id, 
+        vote=0
+    )
+
+    abstain_votes = BillVoting.objects.all().filter(
+        bill_legislation_synopsis_id=billlegislationsynopsis_id, 
+        vote=2
+    )
+
+    absent_votes = BillVoting.objects.all().filter(
+        bill_legislation_synopsis_id=billlegislationsynopsis_id, 
+        vote=3
+    )
+
+    return render(request, template_name,
+        {'current_bill': current_bill,
+        'yes_votes': yes_votes,
+        'no_votes': no_votes,
+        'abstain_votes': abstain_votes,
+        'absent_votes': absent_votes}
+    )
---
+++ nashlegislature/website/views/resovoting_view.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from website.models import ResolutionLegislationSynopsis, ResolutionVoting
+
+
+def resolution_voting(request, resolutionlegislationsynopsis_id):
+    """
+    purpose: gets the resolution voting data and displays on resovoting.html.
+    author: James Tonkin
+    args: request allows Django to see session data
+    returns: Combines resovoting.html with resolution voting dictionaries and returns
+    the request with that rendered text.
+    """
+    template_name = 'resovoting.html'
+    current_resolution = ResolutionLegislationSynopsis.objects.get(pk=resolutionlegislationsynopsis_id)
+
+    yes_votes = ResolutionVoting.objects.all().filter(
+        resolution_legislation_synopsis_id=resolutionlegislationsynopsis_id, 
+        vote=1
+    )
+
+    no_votes = ResolutionVoting.objects.all().filter(
+        resolution_legislation_synopsis_id=resolutionlegislationsynopsis_id, 
+        vote=0
+    )
+
+    abstain_votes = ResolutionVoting.objects.all().filter(
+        resolution_legislation_synopsis_id=resolutionlegislationsynopsis_id, 
+        vote=2
+    )
+
+    absent_votes = ResolutionVoting.objects.all().filter(
+        resolution_legislation_synopsis_id=resolutionlegislationsynopsis_id, 
+        vote=3
+    )
+
+    return render(request, template_name,
+        {'current_resolution': current_resolution,
+        'yes_votes': yes_votes,
+        'no_votes': no_votes,
+        'abstain_votes': abstain_votes,
+        'absent_votes': absent_votes}
+    )
