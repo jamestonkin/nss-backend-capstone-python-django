bbbd81d1014d45624b64c95e7d43ac9f10213f95
James Tonkin <jameswtonkin@gmail.com>
2017-07-11 19:49:17 -0500

    working on vote data scraping

--- nashlegislature/scraping/votingscrape.py
+++ nashlegislature/scraping/votingscrape.py
@@ -34,69 +34,68 @@ for i in minutes_2016:
 for i in minutes_2015[:6]:
     minutes_link_list.append([i['href']])
 
-print(minutes_link_list)
 
-for link in minutes_link_list:
-    # print(link[0])
-    url_for_minutes_page = link[0]
-    headers = {
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
+# for link in minutes_link_list:
+url_for_minutes_page = "http://www.nashville.gov/mc/minutes/term_2015_2019/2016_09_20_minutes.htm"
+# url_for_minutes_page = link[0]
+headers = {
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
 
-    response = requests.get(url_for_minutes_page, headers=headers)
+response = requests.get(url_for_minutes_page, headers=headers)
 
-    if response.status_code == 200:
-        html = BeautifulSoup(response.content, 'html.parser')
-    else:
-        print("ERROR! Site is not loading!")
+if response.status_code == 200:
+    html = BeautifulSoup(response.content, 'html.parser')
+else:
+    print("ERROR! Site is not loading!")
 
-    """
-    Program will script the count votes page.
-    """
+"""
+Program will script the count votes page.
+"""
 
-    # Get all the <a>'s
-    a_tags = html.find_all('a')
+# Get all the <a>'s
+a_tags = html.find_all('a')
 
-    vote_list = list()
-    id_list = list()
+vote_list = list()
+id_list = list()
 
-    for bills in a_tags:
-        each_bill = bills.text.split(' ')
-        if each_bill[-1] == '':
-            del each_bill[-1]
+for bills in a_tags:
+    each_bill = bills.text.split(' ')
+    if each_bill[-1] == '':
+        del each_bill[-1]
 
-        del each_bill[:-1]
-        # print(each_bill)
+    del each_bill[:-1]
+    # print(each_bill)
 
-    for i in a_tags:
+for i in a_tags:
+    # The resolution summary is the very next <p> of the <a>.
+    p_tag = i.find_next('p')
+    p_content = p_tag.text
 
-        # The resolution summary is the very next <p> of the <a>.
-        p_tag = i.find_next('p')
-        p_content = p_tag.text
+    id_num = re.compile("(BL)?(RS)?[0-9]{4}")
+    if id_num.search(p_content):
+        print('yes we have IDs', p_content)
+        id_list.append([p_content])
 
-        id_num = re.compile("(BL)?(RS)?[0-9]{4}")
-        if id_num.search(p_content):
-            print('yes we have IDs', p_content)
-            id_list.append([p_content])
-        # Making sure that the summary contains votes looking for the "Ayes" in the content.
-        ayes = re.compile("Ayes")
-        if ayes.search(p_content):
-            print('yes we have ayes')
-            vote_list.append([p_content])
+    # Making sure that the summary contains votes looking for the "Ayes" in the content.
+    ayes = re.compile("Ayes")
+    if ayes.search(p_content):
+        print('yes we have ayes')
+        vote_list.append([p_content])
 
-    # # Counting "Ayes" votes
-    # for i in vote_list:
-    #     ayes_count = list()
-    #     split_sentence = i[0].split(' ')
-    #     ayes_key = split_sentence.index('“Ayes”')
-    #     for v in split_sentence[ayes_key:]:
-    #         if v == '“Noes”':
-    #             break
-    #         if v != '':
-    #             ayes_count.append(v)
-    #     del ayes_count[-1]
-    #     del ayes_count[0]
-        # print(ayes_count)
-        # print(len(ayes_count) - 1)
-    print(id_list)
-    print(vote_list)
+# # Counting "Ayes" votes
+# for i in vote_list:
+#     ayes_count = list()
+#     split_sentence = i[0].split(' ')
+#     ayes_key = split_sentence.index('“Ayes”')
+#     for v in split_sentence[ayes_key:]:
+#         if v == '“Noes”':
+#             break
+#         if v != '':
+#             ayes_count.append(v)
+#     del ayes_count[-1]
+#     del ayes_count[0]
+    # print(ayes_count)
+    # print(len(ayes_count) - 1)
+print(id_list)
+# print(vote_list)
 
